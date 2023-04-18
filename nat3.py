4e63401adb40240d7e4632b4c28f2c02d72617a4
KHemanth2001 <130360139+KHemanth2001@users.noreply.github.com>
2023-04-18 20:22:04 +0530

    Update nat3.py

--- nat3.py
+++ nat3.py
@@ -7,7 +7,7 @@ import pandas as pd
 import os
 
 DIR_PATH = os.path.abspath(os.path.dirname(__file__))
-path=DIR_PATH
+path=DIR_PATH+'\_Output'
 print(path)
 try:
      os.mkdir(path)
@@ -15,18 +15,20 @@ except OSError as error:
      print(error)
 options = webdriver.ChromeOptions();
 options.add_experimental_option("prefs",{"download.default_directory" : path})
-driver = webdriver.Chrome(service_log_path="D:/selenium/chromedriver.exe",chrome_options=options)
+driver = webdriver.Chrome(service_log_path="D:/sele/chromedriver.exe",chrome_options=options)
 driver.get("https://www.cores.es/en/estadisticas")
 driver.maximize_window()
 time.sleep(5)
 driver.find_element(By.CSS_SELECTOR,"a[title='gas-imports.xlsx']").send_keys(Keys.ENTER)
 time.sleep(5)
-
+#
 p=os.path.abspath('gas-imports.xlsx')
-print(p)
+# print(p)
 
 df = pd.read_excel(p, sheet_name='Africa',usecols='A:Q',header=5)
 df=df.iloc[:248]
-csv_path=path+'\_Output\Gas-Imports.csv'
+csv_path=path+'\Gas-Imports.csv'
+print(csv_path)
+# print("C:\Users\weakh\PycharmProjects\pythonProject3\_Output")
 df.to_csv(csv_path,index=False)
-print(df.to_string())
+# print(df.to_string())
