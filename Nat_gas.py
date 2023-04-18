abb3307f05276b623f4da15f9caa538f25d96b3b
KHemanth2001 <130360139+KHemanth2001@users.noreply.github.com>
2023-04-18 20:31:18 +0530

    Update Nat_gas.py

--- Nat_gas.py
+++ Nat_gas.py
@@ -7,18 +7,27 @@ import pandas as pd
 import os
 
 DIR_PATH = os.path.abspath(os.path.dirname(__file__))
-folder_name="\_Output"
-path=DIR_PATH+folder_name
+path=DIR_PATH+'\_Output'
+print(path)
+try:
+     os.mkdir(path)
+except OSError as error:
+     print(error)
 options = webdriver.ChromeOptions();
-options.add_experimental_option("prefs",{"download.default_directory" : path});
+options.add_experimental_option("prefs",{"download.default_directory" : path})
 
 driver = webdriver.Chrome(service_log_path="D:/selenium/chromedriver.exe",chrome_options=options)
 driver.get("https://www.cores.es/en/estadisticas")
 driver.maximize_window()
 time.sleep(5)
 driver.find_element(By.CSS_SELECTOR,"a[title='gas-imports.xlsx']").send_keys(Keys.ENTER)
+time.sleep(5)
 
-path=DIR_PATH+"\_Output"
-df = pd.read_excel(path+"\gas-imports.xlsx", sheet_name='Africa',usecols='A:Q',header=5)
+p=os.path.abspath('gas-imports.xlsx')
+
+df = pd.read_excel(p, sheet_name='Africa',usecols='A:Q',header=5)
 df=df.iloc[:248]
-df.to_csv('Gas-Imports.csv',index=False)
+csv_path=path+'\Gas-Imports.csv'
+
+df.to_csv(csv_path,index=False)
+df.close()
