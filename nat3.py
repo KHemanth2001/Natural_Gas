040bb5b689b1908a514838cdf63d37f05ab8c340
KHemanth2001 <130360139+KHemanth2001@users.noreply.github.com>
2023-04-18 20:12:08 +0530

    Update nat3.py

--- nat3.py
+++ nat3.py
@@ -4,12 +4,9 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
-# import requests
-# from bs4 import BeautifulSoup
 import os
 
 DIR_PATH = os.path.abspath(os.path.dirname(__file__))
-# folder_name="\_Output"
 path=DIR_PATH
 print(path)
 try:
@@ -24,11 +21,12 @@ driver.maximize_window()
 time.sleep(5)
 driver.find_element(By.CSS_SELECTOR,"a[title='gas-imports.xlsx']").send_keys(Keys.ENTER)
 time.sleep(5)
-# path=DIR_PATH+"\_Output"
+
 p=os.path.abspath('gas-imports.xlsx')
 print(p)
-# print("D:\pycharm\\venv\_Output\gas-imports.xlsx")
+
 df = pd.read_excel(p, sheet_name='Africa',usecols='A:Q',header=5)
 df=df.iloc[:248]
-df.to_csv('Gas-Imports.csv',index=False)
+csv_path=path+'\_Output\Gas-Imports.csv'
+df.to_csv(csv_path,index=False)
 print(df.to_string())
