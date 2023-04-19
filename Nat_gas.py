c3ef38a41bf9cfc2020e388e3ffa6af69ba24f9b
KHemanth2001 <130360139+KHemanth2001@users.noreply.github.com>
2023-04-19 14:36:23 +0530

    Update Nat_gas.py

--- Nat_gas.py
+++ Nat_gas.py
@@ -6,28 +6,50 @@ from selenium.webdriver.chrome.options import Options
 import pandas as pd
 import os
 
-DIR_PATH = os.path.abspath(os.path.dirname(__file__))
-path=DIR_PATH+'\_Output'
-print(path)
-try:
-     os.mkdir(path)
-except OSError as error:
-     print(error)
-options = webdriver.ChromeOptions();
-options.add_experimental_option("prefs",{"download.default_directory" : path})
+# DIR_PATH = os.path.abspath(os.path.dirname(__file__))
+# # folder_name="\_Output"
+# path=DIR_PATH
+# # path=DIR_PATH+folder_name
+# options = webdriver.ChromeOptions() ;
+# prefs = {"download.default_directory" : path};
+# options.add_experimental_option("prefs",prefs);
 
-driver = webdriver.Chrome(service_log_path="D:/selenium/chromedriver.exe",chrome_options=options)
-driver.get("https://www.cores.es/en/estadisticas")
-driver.maximize_window()
-time.sleep(5)
-driver.find_element(By.CSS_SELECTOR,"a[title='gas-imports.xlsx']").send_keys(Keys.ENTER)
-time.sleep(5)
+def generatebrowser(chromedriver_url):
+     DIR_PATH = os.path.abspath(os.path.dirname(__file__))
+     # folder_name="\_Output"
+     path = DIR_PATH
+     # path=DIR_PATH+folder_name
+     options = webdriver.ChromeOptions();
+     prefs = {"download.default_directory": path};
+     options.add_experimental_option("prefs", prefs);
+    # options = webdriver.ChromeOptions()
+     driver = webdriver.Chrome(service_log_path=chromedriver_url, options=options)
+     driver.get("https://www.cores.es/en/estadisticas")
+     driver.maximize_window()
+     time.sleep(5)
+     driver.find_element(By.CSS_SELECTOR, "a[title='gas-imports.xlsx']").send_keys(Keys.ENTER)
+     time.sleep(5)
 
-p=os.path.abspath('gas-imports.xlsx')
+def generate_file():
+    path=os.path.abspath('gas-imports.xlsx')
+    df=pd.read_excel(path,sheet_name='Africa',usecols='A:Q',header=5)
+    df=df.loc[:248]
+    DIR_PATH = os.path.abspath(os.path.dirname(__file__))
+    folder_name = "\_Output"
+    path = os.path.join(DIR_PATH, folder_name)
+    try:
+        os.mkdir(path)
+    except OSError as error:
+        print(error)
+    csv_path=path+"\Gas-Imports.csv"
+    print(csv_path)
+    df.to_csv(os.path.join('_Output', 'Gas-Imports.csv'),index=False)
 
-df = pd.read_excel(p, sheet_name='Africa',usecols='A:Q',header=5)
-df=df.iloc[:248]
-csv_path=path+'\Gas-Imports.csv'
 
-df.to_csv(csv_path,index=False)
-driver.close()
+
+
+
+
+generatebrowser("D:/selenium/chromedriver.exe")
+generate_file()
+
